61534ece391af2409e0a76dee506a363294aa3ab
LLLLLI <Tianhaooo.li@outlook.com>
2019-05-08 19:41:45 +0800

    init

---
+++ model/model.py
@@ -0,0 +1,9 @@
+import torch
+
+from net import Net
+
+class Model(object):
+    def __init__(self, opt):
+        self.xx = opt.xx
+        self.xxx = opt.xxx
+        
---
+++ model/net.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        pass
+    
+    def forward(self, input):
+        pass
